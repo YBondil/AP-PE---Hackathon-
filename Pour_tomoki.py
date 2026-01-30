9e0410f85f22def991a0de16b0ba0c7952642272
Eloi Colliau <eloi.colliau@etu.minesparis.psl.eu>
2026-01-30 13:06:08 +0100

    Tomoki fonction

---
+++ Pour_tomoki.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+
+def distance(x,y):
+    return math.sqrt(x**2 + y**2)
+
+def __init__(self, player, ennemies):
+    self.player = player
+    self.ennemies = ennemies
+    self.grid = [[]]
+    self.room = None
+
+def is_there_enemy(self):
+    for enemy in self.ennemies:
+        if self.player.x == enemy.x and self.player.y == enemy.y:
+            return enemy
+    print("No ennemy on your position")
+    return None
+
+def initialize(self):
+    grid[self.player.y][self.player.x] = "@"
+    for enemy in self.ennemies:
+        grid[enemy.y][enemy.x] = "E"
+
+def update(self):
+    self.player.move()
+    self.ennemies.move()
+    if self.is_there_enemy() != None:
+        E = self.is_there_enemy()
+        combat(self.player, E)
